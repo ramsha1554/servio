bdc30829d8a6d8f1c1b521242b3a97cdeaf61d1a
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-01 14:26:16 +0530

    added routes of home and forgot passowrd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,20 @@ import {Routes, Route } from 'react-router-dom';
 import SignUp from './pages/SignUp.jsx';
 import SignIn from './pages/SignIn.jsx';
 export const serverUrl="http://localhost:8000"
+import ForgotPassword from './pages/ForgotPassword.jsx';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import useGetCurrentUser from './hooks/useGetCurrentUser.jsx';
 function App() {
+  const {userData}=useSelector((state)=>state.user)
   return <>
   
   <Routes>
 
-    <Route path="/signup" element={<SignUp />} />
-    
-    <Route path="/signin" element={<SignIn />} />
-
+  <Route path='/signup' element={!userData?<SignUp/>:<Navigate to={"/"}/>}/>
+    <Route path='/signin' element={!userData?<SignIn/>:<Navigate to={"/"}/>}/>
       <Route path='/forgot-password' element={!userData?<ForgotPassword/>:<Navigate to={"/"}/>}/>
+      <Route path='/' element={userData?<Home/>:<Navigate to={"/signin"}/>}/>
 
 
   </Routes>
