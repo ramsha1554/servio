d27bc139ad9682028ecd6036c4c11627bd6bf3e6
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-04 00:02:33 +0530

    useGetShopByCity.jsx

--- frontend/src/hooks/useGetShopByCity.jsx
+++ frontend/src/hooks/useGetShopByCity.jsx
@@ -2,7 +2,6 @@ import axios from 'axios'
 import React, { useEffect } from 'react'
 import { serverUrl } from '../App'
 import { useDispatch, useSelector } from 'react-redux'
-
 import { setShopsInMyCity, setUserData } from '../redux/userSlice'
 
 function useGetShopByCity() {
