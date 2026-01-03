efc6ad1e3d8a5d6014b8b9172df6316d90ff0dd6
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-03 23:54:37 +0530

    user controllr

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -2,11 +2,11 @@ import User from "../models/user.model.js"
 
 export const getCurrentUser=async (req,res) => {
     try {
-        const userId=req.userId// from isAuth middleware  // jaise hum email password lete the req.body se waise hi yaha req.userId le rahe hai jo isAuth middleware me set hua hai
+        const userId=req.userId
         if(!userId){
-            return res.status(400).json({message:"userId is not found"}) //
+            return res.status(400).json({message:"userId is not found"})
         }
-        const user=await User.findById(userId) //
+        const user=await User.findById(userId)
         if(!user){
                return res.status(400).json({message:"user is not found"})
         }
