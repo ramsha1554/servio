8954ea821c18cd0994d9f90753e941b42d2438f9
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-04 00:00:00 +0530

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './redux/store.js'
+import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <Provider store={store}>  
+  <Provider store={store}>
     <App />
   </Provider>
-    </BrowserRouter> 
+  </BrowserRouter>
+ 
 )
-       
