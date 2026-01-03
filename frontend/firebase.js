ec56a608c766c24afaaf2b4ae4c0272ae0d654e3
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-03 23:58:55 +0530

    firebase

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -6,13 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
-    apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "servio-food-delivery-webapp.firebaseapp.com",
-  projectId: "servio-food-delivery-webapp",
-  storageBucket: "servio-food-delivery-webapp.firebasestorage.app",
-  messagingSenderId: "95738714430",
-  appId: "1:95738714430:web:80ba39311e0a025661b46f"
+  apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "vingo-food-delivery.firebaseapp.com",
+  projectId: "vingo-food-delivery",
+  storageBucket: "vingo-food-delivery.firebasestorage.app",
+  messagingSenderId: "693314883513",
+  appId: "1:693314883513:web:4840c943ecf3fb61a33068"
 };
 
 // Initialize Firebase
