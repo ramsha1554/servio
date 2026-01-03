6a73ff0360385b65342a2f0d0d4fa0c9dd6f701a
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-04 00:04:38 +0530

    item.routes.js

--- backend/routes/item.routes.js
+++ backend/routes/item.routes.js
@@ -8,8 +8,12 @@ import { upload } from "../middlewares/multer.js"
 
 const itemRouter=express.Router()
 
-itemRouter.post("/add-item",isAuth,upload.single("image"),addItem) // upload is multer middleware
-itemRouter.post("/edit-item/:itemId",isAuth,upload.single("image"),editItem)// editing item vy item id 
+itemRouter.post("/add-item",isAuth,upload.single("image"),addItem)
+itemRouter.post("/edit-item/:itemId",isAuth,upload.single("image"),editItem)
 itemRouter.get("/get-by-id/:itemId",isAuth,getItemById)
-
+itemRouter.get("/delete/:itemId",isAuth,deleteItem)
+itemRouter.get("/get-by-city/:city",isAuth,getItemByCity)
+itemRouter.get("/get-by-shop/:shopId",isAuth,getItemsByShop)
+itemRouter.get("/search-items",isAuth,searchItems)
+itemRouter.post("/rating",isAuth,rating)
 export default itemRouter
