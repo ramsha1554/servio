fb775cef69d1a00e9d2628d0f2fee34834ee03fb
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-03 23:53:39 +0530

    Update database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1); // Exit process with failure
-  }
-};
-export default connectDB;
+const connectDb=async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("db connected")
+    } catch (error) {
+        console.log("db error")
+    }
+}
+
+export default connectDb
