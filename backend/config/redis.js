6419a113049442eaadd21ad4e127f12abd632d0e
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-08 14:51:37 +0530

    Add Redis configuration

---
+++ backend/config/redis.js
@@ -0,0 +1,15 @@
+import Redis from "ioredis";
+import dotenv from "dotenv"
+dotenv.config()
+
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+redis.on("connect", () => {
+    console.log("Redis connected");
+});
+
+redis.on("error", (err) => {
+    console.log("Redis connection error", err);
+});
+
+export default redis;
