3fe0b6bbb6e6cbd289790e42c76c55cfc5e66b77
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-08 18:31:21 +0530

    added helmet express rate limiter

--- backend/index.js
+++ backend/index.js
@@ -13,37 +13,53 @@ import orderRouter from "./routes/order.routes.js"
 import http from "http"
 import { Server } from "socket.io"
 import { socketHandler } from "./socket.js"
+import "./workers/email.worker.js" // Start email worker for order confirmation 
+import helmet from "helmet"
+import rateLimit from "express-rate-limit"
 
-const app=express()
-const server=http.createServer(app)
+const app = express()
+const server = http.createServer(app)
 
-const io=new Server(server,{
-   cors:{
-    origin:"http://localhost:5173",
-    credentials:true,
-    methods:['POST','GET']
-}
+const io = new Server(server, {
+    cors: {
+        origin: process.env.CLIENT_URL || "http://localhost:5173",
+        credentials: true,
+        methods: ['POST', 'GET']
+    }
 })
 
-app.set("io",io)
+app.set("io", io)
 
 
 
-const port=process.env.PORT || 5000
+const port = process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true
 }))
+
+app.use(helmet())
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
+
 app.use(express.json())
 app.use(cookieParser())
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use("/api/shop",shopRouter)
-app.use("/api/item",itemRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
+app.use("/api/shop", shopRouter)
+app.use("/api/item", itemRouter)
+app.use("/api/order", orderRouter)
 
 socketHandler(io)
-server.listen(port,()=>{
+server.listen(port, () => {
     connectDb()
     console.log(`server started at ${port}`)
 })
