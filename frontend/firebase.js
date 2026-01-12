b75fe9d5bf6821611cd09e0efafc254119365e11
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-12 23:24:54 +0530

    Update Firebase configuration

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,17 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "vingo-food-delivery.firebaseapp.com",
-  projectId: "vingo-food-delivery",
-  storageBucket: "vingo-food-delivery.firebasestorage.app",
-  messagingSenderId: "693314883513",
-  appId: "1:693314883513:web:4840c943ecf3fb61a33068"
+  apiKey: "AIzaSyC3mCvMreGeur22pG-yIWQKbvXfuXMHqUI",
+  authDomain: "servio-food-delivery-webapp.firebaseapp.com",
+  projectId: "servio-food-delivery-webapp",
+  storageBucket: "servio-food-delivery-webapp.firebasestorage.app",
+  messagingSenderId: "95738714430",
+  appId: "1:95738714430:web:80ba39311e0a025661b46f"
 };
 
 // Initialize Firebase
