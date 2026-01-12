0ec0f04640aba31d4d790a74741b3d6636b38ca9
ramsha132 <work.rm.13.dec@gmail.com>
2026-01-12 23:27:32 +0530

    added the tanstack query

--- frontend/src/hooks/useGetMyShop.jsx
+++ frontend/src/hooks/useGetMyShop.jsx
@@ -1,26 +1,32 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { serverUrl } from '../App'
+import { useQuery } from '@tanstack/react-query'
 import { useDispatch, useSelector } from 'react-redux'
-import { setUserData } from '../redux/userSlice'
 import { setMyShopData } from '../redux/ownerSlice'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { serverUrl } from '../App'
 
 function useGetMyshop() {
-    const dispatch=useDispatch()
-    const {userData}=useSelector(state=>state.user)
-  useEffect(()=>{
-  const fetchShop=async () => {
-    try {
-           const result=await axios.get(`${serverUrl}/api/shop/get-my`,{withCredentials:true})
-            dispatch(setMyShopData(result.data))
-  
-    } catch (error) {
-        console.log(error)
+  const dispatch = useDispatch()
+  const { userData } = useSelector(state => state.user)
+
+  const fetchMyShop = async () => {
+    const result = await axios.get(`${serverUrl}/api/shop/get-my`, { withCredentials: true })
+    return result.data
+  }
+
+  const { data } = useQuery({
+    queryKey: ['myShop', userData?._id],
+    queryFn: fetchMyShop,
+    // Only fetch if user is logged in AND is an owner otherwise return null
+    enabled: !!userData && userData.role === 'owner',
+    staleTime: 5 * 60 * 1000,
+  })
+
+  useEffect(() => {
+    if (data) {
+      dispatch(setMyShopData(data))
     }
-}
-fetchShop()
- 
-  },[userData])
+  }, [data, dispatch])
 }
 
 export default useGetMyshop
