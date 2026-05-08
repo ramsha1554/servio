2900bf10d5b57752f6375b852bc5c8cac6bd157e
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-08 23:11:14 +0530

    fix: u to U

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
-import UserDashboard from '../components/userDashboard'
+import UserDashboard from '../components/UserDashboard'
 import OwnerDashboard from '../components/OwnerDashboard'
 import DeliveryBoy from '../components/DeliveryBoy'
 
