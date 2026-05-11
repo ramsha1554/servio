94c0f7c7057308d39a9acd5f096ca20fcfd3a44c
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-11 23:33:00 +0530

    refactor: complete removal of Leaflet and migration to Google Maps embed

--- frontend/src/components/DeliveryBoyTracking.jsx
+++ frontend/src/components/DeliveryBoyTracking.jsx
@@ -1,57 +1,30 @@
 import React from 'react'
-import scooter from "../assets/scooter.png"
-import home from "../assets/home.png"
-import "leaflet/dist/leaflet.css"
-import L from "leaflet"
-import { MapContainer, Marker, Polyline, Popup, TileLayer } from 'react-leaflet'
-const deliveryBoyIcon = new L.Icon({
-    iconUrl: scooter,
-    iconSize: [40, 40],
-    iconAnchor: [20, 40]
-})
-const customerIcon = new L.Icon({
-    iconUrl: home,
-    iconSize: [40, 40],
-    iconAnchor: [20, 40]
-})
+import { IoLocationSharp } from "react-icons/io5";
+
 function DeliveryBoyTracking({ data }) {
-
-    const deliveryBoyLat = data.deliveryBoyLocation.lat
-    const deliveryBoylon = data.deliveryBoyLocation.lon
-    const customerLat = data.customerLocation.lat
-    const customerlon = data.customerLocation.lon
-
-    const path = [
-        [deliveryBoyLat, deliveryBoylon],
-        [customerLat, customerlon]
-    ]
-
-    const center = [deliveryBoyLat, deliveryBoylon]
+    const deliveryBoyLat = data?.deliveryBoyLocation?.lat;
+    const deliveryBoyLon = data?.deliveryBoyLocation?.lon;
 
     return (
-        <div className='w-full h-[400px] mt-3 rounded-xl overflow-hidden shadow-md'>
-            <MapContainer
-                className={"w-full h-full"}
-                center={center}
-                zoom={16}
-            >
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        <div className='w-full h-[400px] mt-3 rounded-xl overflow-hidden shadow-md border border-gray-200'>
+            {deliveryBoyLat && deliveryBoyLon ? (
+                <iframe
+                    width="100%"
+                    height="100%"
+                    style={{ border: 0 }}
+                    loading="lazy"
+                    allowFullScreen
+                    src={`https://maps.google.com/maps?q=${deliveryBoyLat},${deliveryBoyLon}&z=16&output=embed`}
                 />
-             <Marker position={[deliveryBoyLat,deliveryBoylon]} icon={deliveryBoyIcon}>
-             <Popup>Delivery Boy</Popup>
-             </Marker>
-              <Marker position={[customerLat,customerlon]} icon={customerIcon}>
-             <Popup>Delivery Boy</Popup>
-             </Marker>
-
-
-<Polyline positions={path} color='blue' weight={4}/>
-
-            </MapContainer>
+            ) : (
+                <div className='h-full flex flex-col items-center justify-center text-gray-400 bg-gray-50 gap-2'>
+                    <IoLocationSharp size={32} className='text-gray-300' />
+                    <p className='text-sm'>Waiting for delivery partner's location...</p>
+                </div>
+            )}
         </div>
     )
 }
 
 export default DeliveryBoyTracking
+
